f9c2ffcdf0d578c3837e3d547fdf248bcfede859
Jonathan Glines <auntieNeo@gmail.com>
2014-05-25 20:21:42 -0600

    Added packages. Disable console blanking. Set timezone.

--- configuration.nix
+++ configuration.nix
@@ -14,8 +14,11 @@
   boot.loader.gummiboot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # See console messages during early boot
+  # See console messages during early boot.
   boot.initrd.kernelModules = [ "fbcon" ];
+
+  # Disable console blanking after being idle.
+  boot.kernelParams = [ "consoleblank=0" ];
 
   networking.hostName = "hakase"; # Define your hostname.
   networking.wireless.enable = true;  # Enables wireless.
@@ -33,15 +36,25 @@
     defaultLocale = "en_US.UTF-8";
   };
 
+  # Set the timezone.
+  time.timeZone = "US/Mountain";
+
   # List packages installed in system profile. To search by name, run:
   # nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
 #    ansible  # TODO: write an ansible package
+    anki
+#    anthy  # TODO: write an anthy package (with ibus)
     chromium
     cmake
+    conky
     dmenu
+    evince
     git
+    gutenprint
     irssi
+    linuxPackages.virtualbox
+    mercurial
     mplayer
     pmutils
     rxvt_unicode
@@ -51,11 +64,12 @@
     sudo
     texLiveFull
     tmux
+#    typespeed  # TODO: write a typespeed package
     vagrant
     valgrind
-    linuxPackages.virtualbox
     vim
     wget
+    xlibs.xinit
   ];
 
   # List services that you want to enable:
@@ -64,12 +78,13 @@
   # services.openssh.enable = true;
 
   # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  services.printing.enable = true;
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.xserver.layout = "dvorak";
   # services.xserver.xkbOptions = "eurosign:e";
+  services.xserver.synaptics.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.extraUsers.auntieneo = {
@@ -102,18 +117,21 @@
 # TODO: patch and install dwm
 # TODO: patch and install rxvt-unicode with shadows
 # TODO: make a package for ansible
+# TODO: write an anthy package
 # TODO: xset r rate 400 once and for all
 # TODO: disable capslock
 # TODO: disable annoying ctrl+s flow control
 # TODO: install aspell
-# TODO: install and configure anthy
-# TODO: install and configure anki
 # TODO: install, configure, and sync unison
 # TODO: fix white-out problem with idle screen
 # TODO: disable suspend on closed lid
 # TODO: configure multiple monitors (depending on which host)
 # TODO: configure backgrounds (depending on which host)
-# TODO: configure wireless (on laptop)
+# TODO: only configure wireless on systems that need it
 # TODO: disable beep
 # TODO: figure out how to organize configuration files
+# TODO: install ssh keys
+# TODO: configure ssh-agent
+# TODO: configure cmus
+# TODO: configure audio (don't break on reboot, change depending on the host)
 }
