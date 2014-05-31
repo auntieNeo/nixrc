c1f6e03c6c689a96b04070f3c02132f35c72e337
Jonathan Glines <auntieNeo@gmail.com>
2014-05-31 14:32:28 -0600

    Changed rebuild script to use git reset --hard, which won't fail with local changes.

--- rebuild.sh
+++ rebuild.sh
@@ -2,5 +2,5 @@
 GIT="git -c user.email='none@example.com' -c user.name='none'"
 cd /etc/nixos/
 sudo $GIT fetch local master
-sudo $GIT checkout local/master
+sudo $GIT reset --hard
 sudo nixos-rebuild switch
