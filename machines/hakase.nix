bdaae3ee04cb7604524f8f420fc5ed6c7f20a301
Jonathan Glines <auntieNeo@gmail.com>
2014-06-01 21:45:14 -0600

    Moved TrackPoint configuration into laptop profile.

--- machines/hakase.nix
+++ machines/hakase.nix
@@ -16,18 +16,5 @@
     deviceSection = ''
       Option "RenderAccel" "false"
     '';
-
-    # Enable vertical and horizontal TrackPoint scrolling.
-    config = ''
-      Section "InputClass"
-        Identifier "Trackpoint Wheel Emulation"
-        MatchProduct "TPPS/2 IBM TrackPoint|DualPoint Stick|Synaptics Inc. Composite TouchPad / TrackPoint|ThinkPad USB Keyboard with TrackPoint|USB Trackpoint pointing device|Composite TouchPad / TrackPoint"
-        MatchDevicePath "/dev/input/event*"
-        Option "EmulateWheel" "true"
-        Option "EmulateWheelButton" "2"
-        Option "XAxisMapping" "6 7"
-        Option "YAxisMapping" "4 5"
-      EndSection
-    '';
   };
 }
