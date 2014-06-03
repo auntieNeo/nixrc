26074e317dd6bbabc3c8410c3a65bd6a2856f277
Jonathan Glines <auntieNeo@gmail.com>
2014-06-03 11:35:08 -0600

    Added lain and also a .nix file for the default nixos hostname.

--- machines/nixos.nix
+++ machines/nixos.nix
@@ -4,12 +4,11 @@
   imports =
     [
       ../profiles/desktop.nix
-      ../profiles/server.nix
     ];
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
   # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/disk/by-id/wwn-0x50014ee0ad9ca4a8";
+  #boot.loader.grub.device = "/dev/sda";
 }
