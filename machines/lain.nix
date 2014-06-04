cbdfb0240169dc6be69a21b91146399d6751b72b
Jonathan Glines <auntieNeo@gmail.com>
2014-06-03 21:02:37 -0600

    Multi-head working on lain.

--- machines/lain.nix
+++ machines/lain.nix
@@ -14,40 +14,25 @@
   boot.loader.grub.device = "/dev/disk/by-id/wwn-0x50014ee0ad9ca4a8";
 
   services.xserver = {
-    # Device-ati[0]
-    deviceSection = ''
-      Option "Monitor[0]" "DisplayPort-0"
-      Option "Monitor[1]" "DVI-0"
-      Option "Monitor[2]" "DVI-1"
-    '';
-
-    # Actually Monitor[0]
-    monitorSection = ''
-    '';
-
     config = ''
       Section "Monitor"
-        Identifier "Monitor[1]"
+        Identifier "DisplayPort-0"
+        Option "Primary" "true"
+        Option "PreferredMode" "1920x1080"
+      EndSection
+
+      Section "Monitor"
+        Identifier "DVI-0"
+        Option "PreferredMode" "1680x1050"
         Option "Rotate" "left"
         Option "RightOf" "DisplayPort-0"
       EndSection
 
       Section "Monitor"
-        Identifier "Monitor[2]"
+        Identifier "DVI-1"
+        Option "PreferredMode" "1680x1050"
         Option "Rotate" "left"
         Option "RightOf" "DVI-0"
-      EndSection
-
-      Section "Screen"
-        Identifier "Screen-ati[1]"
-        Device "Device-ati[0]"
-        Monitor "Monitor[1]"
-      EndSection
-
-      Section "Screen"
-        Identifier "Screen-ati[2]"
-        Device "Device-ati[0]"
-        Monitor "Monitor[2]"
       EndSection
     '';
   };
