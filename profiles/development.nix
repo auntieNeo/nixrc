26c4215e1e17d90d2399b1deabd0cf6266627543
Jonathan Glines <auntieNeo@gmail.com>
2014-06-24 18:58:50 -0600

    Removed submodule (it was on the wrong commit)

---
+++ profiles/development.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ...}:
+
+{
+  # install development packages
+  environment.systemPackages = with pkgs; [
+    netbeans
+  ];
+
+  system.activationScripts =
+  {
+    # install development environments
+    dev_environments = ''
+      if [ ! -d /home/auntieneo/.nixpkgs/environments ]; then
+        mkdir -p /home/auntieneo/.nixpkgs/environments
+      fi
+      for f in ${../dotfiles/nixpkgs/environments}/*.nix; do
+        ln -fs $f /home/auntieneo/.nixpkgs/environments/
+      done
+    '';
+  };
+}
