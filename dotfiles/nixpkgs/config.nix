d5926f86ec45a8992f727ac7ad43d2d4616f1f5b
Jonathan Glines <auntieNeo@gmail.com>
2014-06-30 18:30:12 -0600

    Fixed multiple-package nixpkgs.config.

--- dotfiles/nixpkgs/config.nix
+++ dotfiles/nixpkgs/config.nix
@@ -1,9 +1,11 @@
 rec {
   home = builtins.getEnv "HOME";
-#  packageOverrides = import "${home}/.nixpkgs/environments/migrate.nix";
-#  packageOverrides = import "${home}/.nixpkgs/environments/opengl.nix";
-#  packageOverrides = import "${home}/.nixpkgs/environments/swc.nix";
-#  packageOverrides = import "${home}/.nixpkgs/environments/wld.nix";
-  packageOverrides = import "${home}/.nixpkgs/environments/tots.nix";
-#  packageOverrides = import "${home}/.nixpkgs/environments/xmonad.nix";
+  packageOverrides = pkgs: {
+    migrate = import "${home}/.nixpkgs/environments/migrate.nix" { pkgs=pkgs; };
+    opengl = (import "${home}/.nixpkgs/environments/opengl.nix" { pkgs=pkgs; }).opengl;
+    swc = import "${home}/.nixpkgs/environments/swc.nix" { pkgs=pkgs; };
+    wld = import "${home}/.nixpkgs/environments/wld.nix" { pkgs=pkgs; };
+    tots = import "${home}/.nixpkgs/environments/tots.nix" { pkgs=pkgs; };
+    xmonad = import "${home}/.nixpkgs/environments/xmonad.nix" { pkgs=pkgs; };
+  };
 }
