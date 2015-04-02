c5c06a42ee011ce75b0699c1b2f43454d9b76c1e
Jonathan Glines <auntieNeo@gmail.com>
2015-04-02 00:34:03 -0600

    Asterisk service close to working. Added sipp derivation.

---
+++ pkgs/sipp/default.nix
@@ -0,0 +1,26 @@
+{ stdenv, fetchurl, autoconf, automake, ncurses }:
+
+stdenv.mkDerivation rec {
+  name = "sipp-${version}";
+  version = "3.4.1";
+
+  src = fetchurl {
+    url = "https://github.com/SIPp/sipp/archive/v${version}.tar.gz";
+    sha256 = "1i1bm1viz9ak6vncqgm8p7gv94i61vj7vkgz11gqp3dgyfhjjs5v";
+  };
+
+  patches = [ ./fix-configure.patch ];
+
+  buildInputs = [ autoconf automake ncurses ];
+
+  preConfigure = ''
+    autoreconf
+  '';
+
+  meta = {
+    description = "Test tool and traffic generator for the SIP protocol";
+    homepage = http://sipp.sourceforge.net/;
+    license = stdenv.lib.licenses.gpl2;
+    maintainers = [ stdenv.lib.maintainers.auntie ];
+  };
+}
--- services/asterisk.nix
+++ services/asterisk.nix
@@ -5,24 +5,62 @@ with lib;
 let
   cfg = config.services.asterisk;
 
+  asteriskUser = "asterisk";
+
   varlibdir = "/var/lib/asterisk";
   spooldir = "/var/spool/asterisk";
   logdir = "/var/log/asterisk";
 
-  asterisk_conf = ''
-    [directories](!)
-    astetcdir => /etc/asterisk
-    astmoddir => /usr/lib/asterisk/modules
-    astvarlibdir => /var/lib/asterisk
-    astdbdir => /var/lib/asterisk
-    astkeydir => /var/lib/asterisk
-    astdatadir => /var/lib/asterisk
-    astagidir => /var/lib/asterisk/agi-bin
-    astspooldir => /var/spool/asterisk
-    astrundir => /var/run/asterisk
-    astlogdir => /var/log/asterisk
-    astsbindir => /usr/sbin
-  '';
+  confFiles = [
+    "acl"
+    "ccss"
+    "cdr"
+    "cel"
+    "extensions"
+    "fetaures"
+    "indications"
+    "manager"
+    "modules"
+    "sip"
+    "stasis"
+    "udptl"
+  ];
+
+  asteriskEtc = pkgs.stdenv.mkDerivation {
+    name = "asterisk.etc";
+
+    asteriskConf = ''
+      [directories]
+      astetcdir => @out@
+      astmoddir => ${pkgs.asterisk}/lib/asterisk/modules
+      astvarlibdir => /var/lib/asterisk
+      astdbdir => /var/lib/asterisk
+      astkeydir => /var/lib/asterisk
+      astdatadir => /var/lib/asterisk
+      astagidir => /var/lib/asterisk/agi-bin
+      astspooldir => /var/spool/asterisk
+      astrundir => /var/run/asterisk
+      astlogdir => /var/log/asterisk
+      astsbindir => ${pkgs.asterisk}/sbin
+    '';
+    extraConf = cfg.extraConfig;
+
+    confFilesString = lib.concatStringsSep " " confFiles;
+
+    extensionsConf = cfg.extensionsConfig;
+    sipConf = cfg.sipConfig;
+
+    buildCommand = ''
+      mkdir -p "$out"
+      echo "$asteriskConf" | sed "s|@out@|$out|g" > "$out"/asterisk.conf
+      echo "$extraConf" >> "$out"/asterisk.conf
+
+      for i in $confFilesString; do
+        conf=$(eval echo "\$${i}Conf")
+        echo "$conf" > "$out"/"$i".conf
+      done
+    '';
+  };
 in
 
 {
@@ -36,6 +74,16 @@ in
         '';
       };
 
+      extensionsConfig = mkOption {
+        default = "";
+        type = types.lines;
+        example = ''
+        '';
+        description = ''
+          The contents of the extensions.conf file.
+        '';
+      };
+
       sipConfig = mkOption {
         default = "";
         type = types.lines;
@@ -46,34 +94,50 @@ in
         '';
       };
 
-      extensionsConfig = mkOption {
+      extraConfig = mkOption {
         default = "";
         type = types.lines;
         example = ''
         '';
         description = ''
-          The contents of the extensions.conf file.
+          Extra configuration options appended to the asterisk.conf file.
         '';
       };
     };
   };
 
   config = mkIf cfg.enable {
+    users.extraUsers = singleton
+    { name = asteriskUser;
+#      uid = config.ids.uids.asterisk;  # FIXME
+      uid = 2000;
+      description = "Asterisk daemon user";
+      home = varlibdir;
+    };
+
     systemd.services.asterisk = {
       description = ''
         Asterisk PBX server
       '';
 
+      serviceConfig = {
+      };
+
       preStart = ''
+        # Copy skeleton directory tree to /var
         for d in '${varlibdir}' '${spooldir}' '${logdir}'; do
+          rm -rf "$d" # FIXME: This is for debugging
+          # TODO: Make exceptions for directories that should be overwritten
           if [ ! -e "$d" ]; then
-            cp -r ${pkgs.asterisk}/"$d" "$d"
+            cp --recursive ${pkgs.asterisk}/"$d" "$d"
+            chown --recursive ${asteriskUser} "$d"
+            find "$d" -type d | xargs chmod 0755
           fi
         done
       '';
 
-      script = with cfg; ''
-        ${pkgs.asterisk}/bin/asterisk -v
+      script = ''
+        ${pkgs.asterisk}/bin/asterisk -v -U ${asteriskUser} -C ${asteriskEtc}/asterisk.conf
       '';
     };
   };
