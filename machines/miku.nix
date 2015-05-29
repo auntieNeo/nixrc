8d84727a7603466bf844585fd1f53a2e560d6624
Jonathan Glines <auntieNeo@gmail.com>
2015-05-28 21:31:01 -0600

    miku working now       ../profiles/server.nix

--- machines/miku.nix
+++ machines/miku.nix
@@ -1,9 +1,19 @@
 { config, pkgs, ... }:
 
 {
+  imports =
+    [
+      ../profiles/server.nix
+    ];
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
   # Define on which hard drive you want to install Grub.
-#  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.device = "/dev/disk/by-id/ata-ST380215A_9RW4SX14";
+
+  # Specify port to bind to for SSH reverse tunneling
+  services.ssh-phone-home = {
+    enable = true;
+    bindPort = 2211;
+  };
 }
