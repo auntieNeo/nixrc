58df593e507a287d82b680f8fa4451e515ccb100
Jonathan Glines <auntieNeo@gmail.com>
2015-09-26 23:39:50 -0600

    Mesa git building

--- profiles/mesa.nix
+++ profiles/mesa.nix
@@ -1,5 +1,16 @@
 { config, lib, pkgs, ... }:
 
+let
+  libdrm_2_4_65 = pkgs: (pkgs.libdrm.overrideDerivation (attrs: rec {
+    name = "libdrm-${version}";
+    version = "2.4.65";
+
+    src = pkgs.fetchurl {
+      url = "http://dri.freedesktop.org/libdrm/${name}.tar.bz2";
+      sha256 = "1i4n7mz49l0j4kr0dg9n1j3hlc786ncqgj0v5fci1mz7pp40m5ki";
+    };
+  }));
+in
 {
   environment.systemPackages = with pkgs; [
 #    piglit
@@ -15,7 +26,8 @@
 
     piglit-patched = (import <nixpkgs> { config.packageOverrides = pkgs: rec {
       waffle = pkgs.callPackage ../pkgs/waffle/default.nix { };
-      mesa = (import <nixpkgs> { config.packageOverrides = pkgs: rec {
+
+      mesa_override_env = (import <nixpkgs> { config.packageOverrides = pkgs: rec {
         # Inside here, we create an environment in which we override mesa and
         # some of the packages that Mesa depends on.
         mesa_noglu = lib.makeOverridable (args: lib.overrideDerivation pkgs.mesa_noglu (attrs: rec {
@@ -31,17 +43,11 @@
           nativeBuildInputs = [ pkgs.pythonPackages.Mako ] ++ attrs.nativeBuildInputs;
         })) {};
 
-        libdrm = pkgs.libdrm.overrideDerivation (attrs: rec {
-          name = "libdrm-${version}";
-          version = "2.4.65";
-
-          src = pkgs.fetchurl {
-            url = "http://dri.freedesktop.org/libdrm/${name}.tar.bz2";
-            sha256 = "1i4n7mz49l0j4kr0dg9n1j3hlc786ncqgj0v5fci1mz7pp40m5ki";
-          };
-        });
+        libdrm = libdrm_2_4_65 pkgs;
         
-      }; }).mesa;
+      }; });
+      mesa = mesa_override_env.mesa;
+      mesa_drivers = mesa_override_env.mesa_drivers;
     }; }).callPackage ../pkgs/piglit/default.nix {};
 
 #    piglit-test = pkgs.callPackage mesa/piglits/master.nix { piglit = piglit-patched; };
